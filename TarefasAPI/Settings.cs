064900ce66822e733252f8e85d3ce1a313e17e28
Altair Noberto <altairnoberto@outlook.com.br>
2024-08-05 09:12:29 -0300

    Update Settings.cs

--- TarefasAPI/Settings.cs
+++ TarefasAPI/Settings.cs
@@ -5,6 +5,6 @@
         // Chave de segurança para criação da credencial dos tokens JWT
         public static string Key = "OQ47tD&m5Zhd8dXs1YcU5SkCjJ9lG7mup9dk34LHUdy5fY0hN@yeq4iC7m3nJjUCi";
         // Endereço onde está hospedado o front-end
-        public static string Cors = "http://127.0.0.1:5500";
+        public static string Cors = "http://localhost:5500";
     }
 }
